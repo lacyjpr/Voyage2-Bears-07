147cf75ea3b2aba466063040a0057e61a7b9bdc8
Jean-Paul Lacy <lacyjpr@gmail.com>
2017-10-10 09:26:19 -0700

    Add autocomple & Geocode location

--- src/components/search.js
+++ src/components/search.js
@@ -4,14 +4,17 @@ import React from "react";
 import Geosuggest from "react-geosuggest";
 // import { connect } from "react-redux";
 //import * as actions from "actions";
+import "./search.css";
 
 export class Search extends React.Component {
   onSuggestSelect(suggest) {
     console.log(suggest);
+    let latlng = suggest.location;
+    console.log(latlng);
   }
 
   onSuggestNoResults(userInput) {
-    console.log("onSuggestNoResults for :" + userInput); // eslint-disable-line
+    console.log("onSuggestNoResults for :" + userInput);
   }
 
   render() {
