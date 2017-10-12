494e77ac3242929aeb084567a362a96e3e2dd1e3
Jean-Paul Lacy <lacyjpr@gmail.com>
2017-10-12 00:57:19 -0700

    Set up Redux
    Fix prettier
    
    Merge branch 'set-up-redux' of https://github.com/chingu-coders/Voyage2-Bears-07 into set-up-redux
    
    Set up Redux
    
    Move store configuration to index.js
    
    Clean up code
    
    Set up Redux
    
    Clean up code
    
    Restructure reducers
    
    Remove a comment

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { configure } from './store/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configure();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+//TODO: Import reduvers here
+
+export const reducer = combineReducers({
+  //TODO: Add reducers here
+});
