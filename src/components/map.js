3cd378a7ccaec355f6c3cecf740821d8f86517da
Jean-Paul Lacy <lacyjpr@gmail.com>
2017-10-16 12:57:34 -0700

    Center map on search

--- src/components/map.js
+++ src/components/map.js
@@ -2,17 +2,22 @@
 
 import React from 'react';
 import { connect } from 'react-redux';
-// import * as actions from './../actions/actions';
 
 class Map extends React.Component {
   constructor(props) {
     super(props);
-    console.log(this.props.center);
     this.state = {
       center: this.props.center,
     };
   }
   componentDidMount() {
+    this.map = new google.maps.Map(this.refs.map, {
+      center: this.state.center,
+      zoom: 2,
+    });
+  }
+
+  componentDidUpdate() {
     this.map = new google.maps.Map(this.refs.map, {
       center: this.state.center,
       zoom: 2,
