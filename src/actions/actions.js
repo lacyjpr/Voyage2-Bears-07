c7a5ee422c7706650a210c5b5ca57e0a4e6e41e9
Jean-Paul Lacy <lacyjpr@gmail.com>
2017-10-22 08:44:11 -0700

    Fixed a merge deletion

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,5 +1,19 @@
 import { firebaseRef } from '../firebase';
 
+export const getRadius = radius => {
+  return {
+    type: 'SET_RADIUS',
+    radius,
+  };
+};
+
+export const getCenter = center => {
+  return {
+    type: 'SET_CENTER',
+    center,
+  };
+};
+
 export const login = uid => {
   return {
     type: 'LOGIN',
