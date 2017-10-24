da29f98de414c83724d40340d47a00e8cb363c88
Jean-Paul Lacy <lacyjpr@gmail.com>
2017-10-24 09:50:12 -0700

    Attemp to add markers

--- src/components/map.js
+++ src/components/map.js
@@ -12,10 +12,33 @@ class Map extends React.Component {
   }
 
   componentDidUpdate() {
+    let markers = this.props.users;
+    //let bounds = new google.maps.LatLngBounds();
+    console.log(markers);
     this.map = new google.maps.Map(this.refs.map, {
       center: this.props.center,
       zoom: 2,
     });
+    let marker;
+    // Loop through the array of markers & place each on the map
+    for (let i = 0; i < markers.length; i++) {
+      let position = new google.maps.LatLng(markers[i].latlng);
+      console.log(position);
+      //bounds.extend(position);
+      marker = new google.maps.Marker({
+        position: position,
+        map: this.map,
+      });
+      //this.map.fitBounds(bounds);
+    }
+    // const boundsListener = google.maps.event.addListener(
+    //   this.map,
+    //   'bounds_changed',
+    //   function(event) {
+    //     this.setZoom(14);
+    //     google.maps.event.removeListener(boundsListener);
+    //   }
+    // );
   }
 
   render() {
@@ -30,6 +53,7 @@ class Map extends React.Component {
 const mapStateToProps = state => {
   return {
     center: state.search.center,
+    users: state.users,
   };
 };
 
