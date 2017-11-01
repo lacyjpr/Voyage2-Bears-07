e550263af3ddf3d77fce2c0e8832e2a331b16f25
Jean-Paul Lacy <lacyjpr@gmail.com>
2017-11-01 09:01:27 -0700

    Fix modal showing last user bug
    Change subject to match textarea width
    
    Change textarea css to use className

--- src/components/send.js
+++ src/components/send.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import './send.css';
 
 class Send extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit() {
+    console.log('sent');
+    this.props.onClose();
+  }
+
   render() {
     if (!this.props.showSend) {
       return null;
@@ -9,9 +21,26 @@ class Send extends Component {
     return (
       <div className="backdrop">
         <div className="modal">
-          <p>Send Component</p>
+          <p>Send Message</p>
           <p>Recipient: {this.props.recipientName}</p>
           <p>Sender: {this.props.senderName}</p>
+          <form onSubmit={this.handleSubmit}>
+            <input type="hidden" ref="sender" value={this.props.sender} />
+            <input type="hidden" ref="recipient" value={this.props.recipient} />
+            <div>
+              <input
+                type="text"
+                className="edit-subject"
+                ref="subject"
+                placeholder="Subject"
+                required
+              />
+            </div>
+            <textarea className="message" />
+            <div />
+            <button>Send</button>
+          </form>
+
           <button onClick={this.props.onClose}>Close</button>
         </div>
       </div>
@@ -19,4 +48,13 @@ class Send extends Component {
   }
 }
 
-export default Send;
+const mapStateToProps = state => {
+  return {
+    users: state.users,
+    filteredUsers: state.filteredUsers,
+    profile: state.profile.profile,
+    auth: state.auth,
+  };
+};
+
+export default connect(mapStateToProps)(Send);
--- src/components/usersList.js
+++ src/components/usersList.js
@@ -1,25 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import Send from './send';
+//import Send from './send';
+import User from './user';
 
 class UsersList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showSend: false,
-    };
-    this.toggleShowSend = this.toggleShowSend.bind(this);
-    console.log(this.props.user);
-  }
-
-  toggleShowSend(e) {
-    e.preventDefault();
-    this.setState({
-      showSend: !this.state.showSend,
-    });
-  }
-
   render() {
     if (this.props.auth.uid) {
       let users = [];
@@ -29,25 +14,28 @@ class UsersList extends Component {
         users = this.props.users;
       }
       console.log(users);
-      let currentUsers = users.map(user => {
-        return (
-          <li key={user.id}>
-            <div className="userlist-username">{user.username}</div>
-            <div className="userlist-location">{user.location}</div>
-            <button onClick={this.toggleShowSend}>Message</button>
-            <Send
-              showSend={this.state.showSend}
-              onClose={this.toggleShowSend}
-              recipient={user.id}
-              recipientName={user.username}
-              sender={this.props.auth.uid}
-              senderName={this.props.profile.username}
-            />
-          </li>
-        );
+      return users.map(user => {
+        return <User key={user.id} user={user} />;
       });
-      console.log('currentUsers: ', currentUsers);
-      return <ul>{currentUsers}</ul>;
+      // let currentUsers = users.map(user => {
+      //   return (
+      //     <li key={user.id}>
+      //       <div className="userlist-username">{user.username}</div>
+      //       <div className="userlist-location">{user.location}</div>
+      //       <button onClick={this.toggleShowSend}>Message</button>
+      //       <Send
+      //         showSend={this.state.showSend}
+      //         onClose={this.toggleShowSend}
+      //         recipient={user.id}
+      //         recipientName={user.username}
+      //         sender={this.props.auth.uid}
+      //         senderName={this.props.profile.username}
+      //       />
+      //     </li>
+      //   );
+      // });
+      // console.log('currentUsers: ', currentUsers);
+      // return <ul>{currentUsers}</ul>;
     } else {
       return <div className="noUser">You must sign in to view users!</div>;
     }
