fc429bbd4d56ee8c198fd6b126c605769b9534b5
Jean-Paul Lacy <lacyjpr@gmail.com>
2017-11-14 11:12:38 -0800

    Change class to classname

--- src/components/signin.js
+++ src/components/signin.js
@@ -11,21 +11,21 @@ class SignIn extends Component {
             className="btn-si btn-facebook"
             onClick={this.props.loginFacebook}
           >
-            <span class="fa fa-facebook" /> Log in with Facebook
+            <span className="fa fa-facebook" /> Log in with Facebook
           </button>
 
           <button
             className="btn-si btn-google"
             onClick={this.props.loginGoogle}
           >
-            <span class="fa fa-google" /> Log in with Google!
+            <span className="fa fa-google" /> Log in with Google!
           </button>
 
           <button
             className="btn-si btn-github"
             onClick={this.props.loginGitHub}
           >
-            <span class="fa fa-github" /> Log in with Github!
+            <span className="fa fa-github" /> Log in with Github!
           </button>
         </div>
 
